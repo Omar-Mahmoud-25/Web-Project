67e7c40e4f9202f866f03d6d0da2e925c850ab93
Omar-Mahmoud-25 <omar.mahmoud25102004@gmail.com>
2024-03-17 20:35:23 +0200

    Added some style

--- snippet.js
+++ snippet.js
@@ -1,6 +1,6 @@
 const target = document.getElementById("nav");
 
 target.innerHTML = 
-  "<!-- Header Section -->\n<div id='navbar'>\n<img src='path_to_your_logo_image' alt='Book Store Logo' />\n<nav>\n<a href='index.html'>Home</a>\n<a href='#about'>About</a>\n<a href='#featured'>Featured</a>\n<a href='#newArrival'>New Arrival</a>\n<a href='#contact'>Contact</a>\n<a href='#reviews'>Reviews</a>\n<a href='#availableBooks'>Available Books</a>\n</nav>\n</div>"
+  "<!-- Header Section -->\n<div id='navbar'>\n<img id = 'logo' src='images/logo.jpeg' alt='Book Store Logo' />\n<nav>\n<a href='index.html'>Home</a>\n<a href='#about'>About</a>\n<a href='#featured'>Featured</a>\n<a href='#newArrival'>New Arrival</a>\n<a href='#contact'>Contact</a>\n<a href='#reviews'>Reviews</a>\n<a href='categories.html'>Available Books</a>\n</nav>\n</div>"
 ;
 console.log(target.innerHTML);
