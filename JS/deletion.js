9e83b62b5032d7175766089e0f324874bec2fa1c
nouraldein66 <111093819+nouraldein66@users.noreply.github.com>
2024-04-19 17:56:26 +0200

    Add files via upload

---
+++ JS/deletion.js
@@ -0,0 +1,35 @@
+function confirmation() {
+    // let confirmMsg = confirm('Are you sure you want to delete this book?');
+    // if(confirmMsg) {
+    //     window.location.href = 'admin-homepage.html';
+    // }
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: "btn btn-success",
+          cancelButton: "btn btn-danger"
+        },
+        buttonsStyling: false
+      });
+      swalWithBootstrapButtons.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes, delete it!",
+        cancelButtonText: "No, cancel!",
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = 'admin-homepage.html';
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire({
+            title: "Cancelled",
+            icon: "error"
+          });
+        }
+      });
+}
+
