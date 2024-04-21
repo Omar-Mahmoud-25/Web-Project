891ad4ed2e2414b070a907bf88894839eb3d6faf
HP USER <loayabelgendy@gmail.com>
2024-04-21 10:45:28 +0200

    fixed add book function in addBook.js

--- JS/addBook.js
+++ JS/addBook.js
@@ -17,7 +17,6 @@ function findBook(name) {
   const jsonData = localStorage.getItem("booksData");
   if (jsonData) {
     const data = JSON.parse(jsonData);
-    printBooks(data);
     for (const key in data) {
       if (data[key].bookName == name) {
         return true;
@@ -27,30 +26,36 @@ function findBook(name) {
   return false;
 }
 
-function printBooks(data) {
-  for (const key in data) {
-    console.log(data[key]);
-  }
-}
-
 function add(name, author, description, category) {
-  books[name] = new Book(name, author, description, category);
-  const jsonData = JSON.stringify(books);
-  localStorage.setItem("booksData", jsonData);
+  if (
+    name.length &&
+    author.length &&
+    description.length &&
+    (category[0] || category[1] || category[2] || category[3])
+  ) {
+    books[name] = new Book(name, author, description, category);
+    const jsonData = JSON.stringify(books);
+    localStorage.setItem("booksData", jsonData);
+    return true;
+  }
+  return false;
 }
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   var found = findBook(name.value);
   if (!found) {
-    add(name.value, author.value, description.value, [
-      checkbox[0].checked,
-      checkbox[1].checked,
-      checkbox[2].checked,
-      checkbox[3].checked,
-    ]);
-    alert("Added Successfully!");
-    window.location.href = "admin-homepage.html";
+    if (
+      add(name.value, author.value, description.value, [
+        checkbox[0].checked,
+        checkbox[1].checked,
+        checkbox[2].checked,
+        checkbox[3].checked,
+      ])
+    ) {
+      alert("Added Successfully!");
+      window.location.href = "admin-homepage.html";
+    } else {alert("Fill all data!");}
   } else {
     alert("This book already exists!");
   }
--- JS/book.js
+++ JS/book.js
@@ -1,5 +1,6 @@
 export default class Book {
-    constructor(bookName, author, description, category) {
+    constructor(bookImage, bookName, author, description, category) {
+        this.bookImage = bookImage;
         this.bookName = bookName;
         this.author = author;
         this.description = description;
