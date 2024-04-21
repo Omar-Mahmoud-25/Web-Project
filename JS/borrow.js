532b15871a5ba3810c77b0d2805df57ea9f02593
HP USER <loayabelgendy@gmail.com>
2024-04-21 20:09:59 +0200

    added borrow book button alert

---
+++ JS/borrow.js
@@ -0,0 +1,53 @@
+class User {
+  constructor(userName, email, password, isAdmin) {
+    this.userName = userName;
+    this.email = email;
+    this.password = password;
+    this.isAdmin = isAdmin;
+  }
+}
+
+let User = sessionStorage.getItem("user");
+User = JSON.parse(User);
+
+function confirmation() {
+  // let confirmMsg = confirm('Are you sure you want to delete this book?');
+  // if(confirmMsg) {
+  //     window.location.href = 'admin-homepage.html';
+  // }
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: "btn btn-success",
+      cancelButton: "btn btn-danger",
+    },
+    buttonsStyling: false,
+  });
+  swalWithBootstrapButtons
+    .fire({
+      title: "Confirm borrowing",
+      text: "",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: User == null ? "Login" : "Borrow",
+      cancelButtonText: "Cancel",
+      reverseButtons: true,
+    })
+    .then((result) => {
+      if (result.isConfirmed && User == null) {
+        window.location.href = "login.html";
+      } else if (result.isConfirmed){
+        swalWithBootstrapButtons.fire({
+            title: "Borrowing Completed",
+            icon: "success",
+          });
+      }else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire({
+          title: "Cancelled",
+          icon: "error",
+        });
+      }
+    });
+}
