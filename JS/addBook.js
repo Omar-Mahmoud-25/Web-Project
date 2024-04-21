995727ad8836fdce9960c90ee0f0865476ef7f33
nouraldein66 <111093819+nouraldein66@users.noreply.github.com>
2024-04-21 18:09:33 +0200

    added class Book in addBook.js

--- JS/addBook.js
+++ JS/addBook.js
@@ -1,4 +1,13 @@
-import Book from "./book.js";
+// import Book from "./book.js";
+class Book {
+  constructor(bookName, author, description, category, img) {
+      this.bookName = bookName;
+      this.author = author;
+      this.description = description;
+      this.category = category;
+      this.img = img;
+  }
+}
 
 var books = {};
 
