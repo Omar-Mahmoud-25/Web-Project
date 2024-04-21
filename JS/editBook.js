782fe4b24724f0c726f0d85b5754c3146a180645
nouraldein66 <111093819+nouraldein66@users.noreply.github.com>
2024-04-21 20:08:58 +0200

    Update editBook.js

--- JS/editBook.js
+++ JS/editBook.js
@@ -1,9 +1,32 @@
-import Book from "./book.js";
+// import Book from "./book.js";
+class Book {
+  constructor(bookName, author, description, category, img) {
+      this.bookName = bookName;
+      this.author = author;
+      this.description = description;
+      this.category = category;
+      this.img = img;
+  }
+}
+
+var data = localStorage.getItem('booksData') ? JSON.parse(localStorage.getItem('booksData')) : {};
+function getParameters(){
+  var url = window.location;
+  var paramsValues = window.location.search;
+
+  var urlParams = new URLSearchParams(paramsValues);
+  let parameter = urlParams.get('name');
+  return parameter;
+}
+
+let param = getParameters();
+var targetBook = data[param];
+
 
 const form = document.getElementById("addForm");
-const name = document.getElementById("name");
-const author = document.getElementById("author");
-const description = document.getElementById("description");
+const name = document.getElementById("name").value = targetBook.bookName;
+const author = document.getElementById("author").value = targetBook.author;
+const description = document.getElementById("description").value = targetBook.description;
 const checkbox = [
   document.getElementById("Romantic"),
   document.getElementById("novel"),
@@ -25,23 +48,23 @@ function findBook(name) {
   return null;
 }
 
-function printBooks(data) {
-  for (const key in data) {
-    // console.log(data[key]);
-  }
-}
+// function printBooks(data) {
+//   for (const key in data) {
+//     // console.log(data[key]);
+//   }
+// }
 
 function edit(name, author, description, category) {
-  var book = new Book(name, author, description, category);
-  const jsonData = JSON.stringify(books);
-  localStorage.setItem("booksData", jsonData);
+  // var book = new Book(name, author, description, category);
+  // const jsonData = JSON.stringify(books);
+  // localStorage.setItem("booksData", jsonData);
 }
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
     var found = findBook(name.value);
     if (!found) {
-      add(name.value, author.value, description.value, [
+      edit(name.value, author.value, description.value, [
         checkbox[0].checked,
         checkbox[1].checked,
         checkbox[2].checked,
