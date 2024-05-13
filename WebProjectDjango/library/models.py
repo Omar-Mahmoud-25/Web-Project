3a1e623c76a4929d5f0bd46003c53e1b82588885
nouraldein66 <111093819+nouraldein66@users.noreply.github.com>
2024-05-13 17:40:03 +0300

    Update models.py

--- WebProjectDjango/library/models.py
+++ WebProjectDjango/library/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    name = models.CharField(max_length=50)
+    author = models.ForeignKey(Author, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    bookImage = models.ImageField(upload_to='images', null=True, blank=True)
+    description = models.CharField(max_length=500)
+    available = models.BooleanField(default=True)
+    def __str__(self):
+        return self.name
