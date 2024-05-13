2d2ee0bf6fd67b162f3c517a539577ca8b92f5c6
HP USER <loayabelgendy@gmail.com>
2024-05-13 20:42:59 +0300

    managed urls and views and updated most html files

--- WebProjectDjango/library/urls.py
+++ WebProjectDjango/library/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path, include
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
+    path("add", views.addBook, name="add"),
+    path("available", views.available, name="available"),
+    path("login", views.login, name="login"),
+    path("signup", views.signup, name="signup"),
 ]
--- WebProjectDjango/library/views.py
+++ WebProjectDjango/library/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render, redirect, get_object_or_404
+# trunk-ignore(ruff/F401)
+from django.shortcuts import get_object_or_404, redirect, render
+
 
 def index(request):
-    return render(request, 'base.html')
+    return render(request, "index.html")
+
+
+def addBook(request):
+    return render(request, "AddBook.html")
+
+
+def available(request):
+    return render(request, "categories.html")
+
+
+def login(request):
+    return render(request, "login.html")
+
+
+def signup(request):
+    return render(request, "signUp.html")
