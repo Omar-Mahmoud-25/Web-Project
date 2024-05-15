5c8db36dbb139b5437715abcc5201e66d2e1837c
nouraldein66 <111093819+nouraldein66@users.noreply.github.com>
2024-05-15 06:29:57 +0300

    Update views.py

--- WebProjectDjango/library/views.py
+++ WebProjectDjango/library/views.py
@@ -1,13 +1,31 @@
 # trunk-ignore(ruff/F401)
 from django.shortcuts import get_object_or_404, redirect, render
+from .models import *
+from .forms import *
 
+BookDictionary = {
+        'books': Book.objects.all(),
+        'suggestions': Book.objects.all()[:5],
+        'form': BookForm()
+    }
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html", BookDictionary)
 
 
 def addBook(request):
-    return render(request, "AddBook.html")
+    if request.method == 'POST':
+        addedBook = BookForm(request.POST, request.FILES)
+        if addedBook.is_valid():
+            addedBook.save()
+            # print(addedBook)
+            # Book.objects.create(name=addedBook.name, author=addedBook.author, category=addedBook.category,
+            #                     bookImage=addedBook.bookImage, description=addedBook.description)
+
+        else:
+            print(addedBook.errors)
+
+    return render(request, "AddBook.html", BookDictionary)
 
 
 def available(request):
