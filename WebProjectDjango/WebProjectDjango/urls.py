46ff6798cf55f93ff8e72376b3ca2adcd03ecd1d
HP USER <loayabelgendy@gmail.com>
2024-05-19 07:22:04 +0300

    fixed urls.py

--- WebProjectDjango/WebProjectDjango/urls.py
+++ WebProjectDjango/WebProjectDjango/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('library.urls')),
+    path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
