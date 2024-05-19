06e5b5495338800e7bdb72507b5f0745cb46f7bf
abdelaziz2642004 <122082712+abdelaziz2642004@users.noreply.github.com>
2024-05-19 07:21:29 +0300

    Update urls.py

--- WebProjectDjango/library/urls.py
+++ WebProjectDjango/library/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("add", views.addBook, name="add"),
-    path("available", views.available, name="available"),
+    path("available", views.AdminAvailable, name="available"),
+    path("Uavailable", views.UserAvailable, name="Uavailable"),
+
     path("login", views.login, name="login"),
     path("signup", views.signup, name="signup"),
     path("validate-username/",
@@ -13,4 +15,9 @@ urlpatterns = [
         name="usernameValidation"),
     path('paginated_books/', views.paginated_books, name='paginated_books'),
     path('book/<int:book_id>/', views.book_details, name='book_details'),
+    path('get-books-by-category/', views.get_books_by_category, name='get_books_by_category'),
+    path('cat_books/', views.cat_books, name='cat_books'),
+
+    
+
 ]
