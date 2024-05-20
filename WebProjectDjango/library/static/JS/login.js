3aae96b8e3de20aa154d71a11cbeb0477b330f2d
Omar-Mahmoud-25 <omar.mahmoud25102004@gmail.com>
2024-05-20 16:30:36 +0300

    سلاش

--- WebProjectDjango/library/static/JS/login.js
+++ WebProjectDjango/library/static/JS/login.js
@@ -27,7 +27,7 @@ form.addEventListener('submit',function(event){
       sessionStorage.setItem('isUserLoggedIn',true);
       sessionStorage.setItem('isAdmin',data.isAdmin);
       console.log(sessionStorage.getItem('isUserLoggedIn'));
-      window.location.href = "";
+      window.location.href = "/";
     }
     else
       console.log('in false'),
